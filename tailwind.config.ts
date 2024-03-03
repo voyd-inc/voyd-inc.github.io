0252c0a6215ea336dbd17e0c995cafd9d11b59ca
D V <divyadityasnaruka@gmail.com>
2024-03-03 19:57:24 +0530

    going live

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,30 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				'nebula-hollow': ['Nebula-Hollow', 'sans-serif'],
+				'nebula-regular': ['Nebula-Regular', 'sans-serif'],
+			},
+			content: {
+				hyphen: "'\u2014'",
+				quote: "'\u201C'",
+			},
+			keyframes: {
+				fadeIn: {
+					'0%': { opacity: '0' },
+					'70%': { opacity: '0' },
+					'100%': { opacity: '1' },
+				},
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;
