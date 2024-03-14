9d85182d9216e0812607cf2bcdf3170f06107e7d
D V <77478658+DarhkVoyd@users.noreply.github.com>
2024-03-14 13:49:31 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ export default function Home() {
     <main className="relative flex flex-col items-center justify-center w-screen min-h-screen font-nebula-regular">
       <div className="w-3/4 md:w-1/2 my-[80px]">
         <h1 className="text-4xl my-4 animate-[fadeIn_0.5s]">Voyd Inc.</h1>
-        <p className="text-base my-4 animate-[fadeIn_1.5s]">The promises whispered in childhood, echoes of a younger heart, promises yet to keep.</p>
         <div className="text-base my-4 rounded-md animate-[fadeIn_2s]">
           <blockquote className="leading-normal before:content-quote before:relative before:text-gray-400 before:text-[30px] before:top-[5px] before:mr-[10px]">
             Discipline is the strongest form of self-love. It&apos;s ignoring something you want right now for something better later on. Discipline reveals the commitment you have to your dreams. Especially on days you don&apos;t want to. The future You is depending on the current You to keep the promises you made to yourself yesterday.
@@ -12,7 +11,7 @@ export default function Home() {
           </blockquote>
         </div>
         <ul className="mt-10">
-          <h2 className="text-2xl my-4 animate-fadeIn animate-[fadeIn_2.5s]">We are building...</h2>
+          <h2 className="text-2xl my-4 animate-fadeIn animate-[fadeIn_2.5s]">Our Work...</h2>
           <li className="before:mr-2 before:content-hyphen before:animate-[fadeIn_2.5s]">
             <Link href="https://enrych.github.io/" className="hover:underline animate-[fadeIn_3s]">Enrich Platforms</Link>
           </li>
